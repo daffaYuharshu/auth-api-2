3f087dc300c3fba7edf58360d1d3e25ab0e921a4
daffaYuharshu <daffayuharshu@gmail.com>
2023-11-29 22:53:01 +0700

    change database config

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize("aiang_db", "root", "aiangadmin12345", {
-  host: "35.186.145.146",
+const db = new Sequelize("aiang_db", "root", "", {
+  host: "localhost",
   dialect: "mysql",
 });
 
